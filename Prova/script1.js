f57c0d9b52882673e4799da51c4da97158732b67
B4ffo <pinto.michele1997@gmail.com>
2023-04-27 17:37:02 +0200

    Test 27/04/2023

---
+++ Prova/script1.js
@@ -0,0 +1,11 @@
+document.getElementById("show").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("Answer1").classList.toggle('show');
+    document.getElementById("Answer2").classList.toggle('show');
+    document.getElementById("Answer3").classList.toggle('show');
+    document.getElementById("Answer4").classList.toggle('show');
+    document.getElementById("Answer5").classList.toggle('show');
+    document.getElementById("Answer6").classList.toggle('show');
+    document.getElementById("Answer7").classList.toggle('show');
+}
